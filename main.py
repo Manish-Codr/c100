b432d90c6de14eaf005a889fe437a01ce6e09d96
Manish-Codr <90592564+Manish-Codr@users.noreply.github.com>
2022-06-13 20:02:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-class Car(object):
-    def __init__(self, color, model, company, topspeed, horsepower):
-        self.color = color
-        self.model = model
-        self.company = company
-        self.topspeed = topspeed
-        self.horsepower = horsepower
+# checkBalance,
+# BalanceEnquiry,
+# CashWithDrawl,
+# InsertCard
+class Atm(object):
+    def __int__(self, cardnumber, pin):
+        self.cardnumber = cardnumber,
+        self.pin = pin
 
-    def start(self):
-        print("started")
+    def checkbalance(self):
+        balance = 500000
+        print("balance = 50,000")
 
-    def stop(self):
-        print("stopped")
-
-    def changeGear(self):
-        print("gear changed")
-
-
-audi = Car("Red", "A6", "Audi", "250", "100")
+    def cashwithdrawl(self, amount):
+        newamount = 50000 - amount
+        print("you have withdrawn amount" + str(amount) + ".your remaining balance is" + str(newamount))
 
 
-def newcar():
-    color = input("Enter The Car Color: ")
-    model = input("Enter The Car Model: ")
-    company = input("Enter The Car Company: ")
-    topspeed = input("Enter The Car Top Speed: ")
-    horsepower = input("Enter The Car HorsePower: ")
-    car = Car(color, model, company, topspeed, horsepower)
-    print(car.color)
-    print(car.model)
-    print(car.company)
-    print(car.topspeed)
-    print(car.horsepower)
+def main():
+    cardnumber = input("Enter Your Card Number: ")
+    pin = input("Enter Your Pin Number: ")
+    newuser = Atm(cardnumber, pin)
+    print("Choose Your Activity: ")
+    print("1. BalanceEnquiry                               2. CashWithDrawl")
+    action = int(input("Enter Activity Number"))
+    if action == 1:
+        newuser.checkbalance()
+    elif action == 2:
+        amount = int(input("Enter The Amount You Want To Withdraw: "))
+        newuser.cashwithdrawl()
+    else:
+        print("Enter A Valid Number")
 
 
-newcar()
+if __name__ == "__main__":
+    main()
